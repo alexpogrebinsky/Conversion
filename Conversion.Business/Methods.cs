4b21d92adc9f9a1c09d174023ccc97d72399d181
Alexander A Pogrebinsky <alex@pogrebinskygroup.com>
2018-04-11 09:49:26 -0400

    working model

---
+++ Conversion.Business/Methods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversion.Business
+{
+    public class Methods
+    {
+        public static double LiterstoGallonsConvert (double gallonsweight)
+        {
+            double gallons = gallonsweight;
+            return (gallonsweight * 3.79);
+
+        }
+
+        public static double GallonstoLitersConvert(double literweight)
+        {
+            double lilters = literweight;
+            return (literweight * 0.79);
+
+        }
+
+    }
+}
--- Conversion/Controllers/HomeController.cs
+++ Conversion/Controllers/HomeController.cs
@@ -3,15 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Conversion.Business;
+using Conversion.Models;
 
 namespace Conversion.Controllers
 {
     public class HomeController : Controller
     {
+       
+
+        public ActionResult Convert(ViewModel vm)
+        {
+            if (vm == null) { return View("convert", new ViewModel { ShowResult = false }); }
+
+            if (vm.Option == 1)
+            {
+                vm.Result = Methods.LiterstoGallonsConvert(vm.Input);
+                vm.OptionName = "Liters to Gallons";
+            }
+            else
+            {
+                vm.Result = Methods.GallonstoLitersConvert(vm.Input);
+                vm.OptionName = "Gallons to Liters";
+            }
+            vm.ShowResult = true;
+
+            ////not needed, just for an example
+            //ViewData.Add("glosrob-example", "A value goes here!");
+
+            return View("convert", vm);
+        }
+
+     
+
         public ActionResult Index()
         {
+
+       
+
+
             return View();
         }
+
+
+
+
+
+
 
         public ActionResult About()
         {
